turris-cz/misc

commit e86724771f8fb19dbcd930c505efccbee7b3e9b6

lxc-generator: add Fedora 37

lxc-generator/generate-lxc.sh
@@ -108,6 +108,8 @@ add_image "Debian" "Bullseye" "aarch64" "`get_lxc_url debian/bullseye/arm64`"
 add_image "Fedora" "35" "aarch64" "`get_lxc_url fedora/35/arm64`"
 add_image "Fedora" "36" "armv7l" "`get_lxc_url fedora/36/armhf`"
 add_image "Fedora" "36" "aarch64" "`get_lxc_url fedora/36/arm64`"
+add_image "Fedora" "37" "armv7l" "`get_lxc_url fedora/37/armhf`"
+add_image "Fedora" "37" "aarch64" "`get_lxc_url fedora/37/arm64`"
 add_image "Gentoo" "openrc" "armv7l" "`get_gentoo_url arm armv7a_hardfp-openrc`"
 add_image "Gentoo" "systemd" "armv7l" "`get_gentoo_url arm armv7a_hardfp-systemd`"
 add_image "Gentoo" "musl-openrc" "armv7l" "`get_gentoo_url arm armv7a_hardfp-musl-openrc`"

commit 6c31099554e47b1c3c9261db9b7b743f696b629d

lxc-generator: Fedora 37 release does not provide armv7 anymore
Reference: https://fedoraproject.org/wiki/Changes/RetireARMv7

Fixes: e86724771f8fb19dbcd930c505efccbee7b3e9b6 ("lxc-generator: add
Fedora 37")

lxc-generator/generate-lxc.sh
@@ -108,7 +108,6 @@ add_image "Debian" "Bullseye" "aarch64" "`get_lxc_url debian/bullseye/arm64`"
 add_image "Fedora" "35" "aarch64" "`get_lxc_url fedora/35/arm64`"
 add_image "Fedora" "36" "armv7l" "`get_lxc_url fedora/36/armhf`"
 add_image "Fedora" "36" "aarch64" "`get_lxc_url fedora/36/arm64`"
-add_image "Fedora" "37" "armv7l" "`get_lxc_url fedora/37/armhf`"
 add_image "Fedora" "37" "aarch64" "`get_lxc_url fedora/37/arm64`"
 add_image "Gentoo" "openrc" "armv7l" "`get_gentoo_url arm armv7a_hardfp-openrc`"
 add_image "Gentoo" "systemd" "armv7l" "`get_gentoo_url arm armv7a_hardfp-systemd`"